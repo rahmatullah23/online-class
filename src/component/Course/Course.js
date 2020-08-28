1a1083a8c2dd48061c33eab0809585dfa3926fac
rahmatullah23 <tomalg@gmail.com>
2020-08-28 13:21:31 +0300

    css add

--- src/component/Course/Course.js
+++ src/component/Course/Course.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Course.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Course = (props) => {
     console.log(props.course)
     return (
-        <div>
+        <div className="course">
             <h3>{props.course.name}</h3>
-            <h3>Fee $ {props.course.price}</h3>
+            <h5>Fee $ {props.course.price}</h5>
             <button className="btn  btn-primary"
               onClick={() =>props.handleAddCourse(props.course)}>
                <FontAwesomeIcon icon={faShoppingCart} /> Enroll Now            
